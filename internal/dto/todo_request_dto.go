e889f3db2b9cf37364a87b0cd753a43c9ff81007
Maheswara Devo <pundadevo21@gmail.com>
2022-10-21 00:34:10 +0800

    fix: fix request body details

--- internal/dto/todo_request_dto.go
+++ internal/dto/todo_request_dto.go
@@ -10,30 +10,13 @@ type TodoRequest struct {
 	Priority  int    `json:"priority"`
 }
 
-type CreateTodoRequest struct {
-	Title     string `json:"title"`
-	Details   string `json:"details"`
-	Completed bool   `json:"completed"`
-	Priority  int    `json:"priority"`
-}
-
 func (tr *TodoRequest) ToEntity() (t *models.Todo) {
 	t = &models.Todo{
 		ID:        tr.ID,
 		Title:     tr.Title,
-		Details:   tr.Title,
+		Details:   tr.Details,
 		Completed: tr.Completed,
 		Priority:  tr.Priority,
-	}
-	return
-}
-
-func (tcr *CreateTodoRequest) ToEntity() (t *models.Todo) {
-	t = &models.Todo{
-		Title:     tcr.Title,
-		Details:   tcr.Details,
-		Completed: tcr.Completed,
-		Priority:  tcr.Priority,
 	}
 	return
 }
--- internal/models/todo.go
+++ internal/models/todo.go
@@ -11,33 +11,4 @@ type Todo struct {
 
 type Todos []*Todo
 
-var TodoList = &Todos{
-	&Todo{
-		ID:        1,
-		Title:     "Test1",
-		Details:   "Test1",
-		Completed: false,
-		Priority:  1,
-	},
-	&Todo{
-		ID:        2,
-		Title:     "Test2",
-		Details:   "Test2",
-		Completed: false,
-		Priority:  1,
-	},
-	&Todo{
-		ID:        3,
-		Title:     "Test3",
-		Details:   "Test3",
-		Completed: false,
-		Priority:  2,
-	},
-	&Todo{
-		ID:        4,
-		Title:     "Test4",
-		Details:   "Test4",
-		Completed: false,
-		Priority:  2,
-	},
-}
+var TodoList = &Todos{}
