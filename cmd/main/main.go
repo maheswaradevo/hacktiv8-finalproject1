88a80a115a24ecb083b1d056c84c4663a88a7baa
Maheswara Devo <pundadevo21@gmail.com>
2022-10-22 19:30:30 +0800

    add new deployed host to swagger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -19,7 +19,7 @@ import (
 // @contact.email soberkoder@swagger.io
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/license/LICENSE-2.0.html
-// @host localhost:8080
+// @host https://todos-api-go.herokuapp.com/
 // @BasePath /
 
 func main() {
